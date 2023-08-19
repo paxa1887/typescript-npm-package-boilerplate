84df8a5b256f095e663228aa6ba52e17dddad600
paxa1887 <paxa1887@gmail.com>
2023-08-19 14:51:34 +0200

    Add template for tests

---
+++ tests/add.test.ts
@@ -0,0 +1,9 @@
+import { add } from '../src/add'
+import {describe, expect} from '@jest/globals';
+
+describe('Add function', function () {
+    it('return sum two numbers', function () {
+      const a = 4, b = 3, result = 7
+      expect(add(a, b) == result);
+    });
+});
