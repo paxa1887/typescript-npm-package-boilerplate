bf3277dbcae0244c7b9477394ab2ebaeb1b24eda
paxa1887 <paxa1887@gmail.com>
2023-08-27 15:46:46 +0200

    Setup typedoc, add template comment

--- src/add.ts
+++ src/add.ts
@@ -1,3 +1,10 @@
+/**
+ * Adds two numbers.
+ *
+ * @param {number} a - The first number to add.
+ * @param {number} b - The second number to add.
+ * @returns {number} The sum of the two numbers.
+ */
 export const add = (a: number, b: number): number => {
   return a + b;
 };
